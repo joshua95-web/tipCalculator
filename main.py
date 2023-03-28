896234a8e4202fabcf63212602b0a09357880818
joshua95-web <joshcscurfield@gmail.com>
2023-03-28 17:59:30 +0100

    added tipPercentage input request, and converted tipPercentage to integer

--- main.py
+++ main.py
@@ -3,3 +3,5 @@ print("welcome to TIPCALCULATOR\n")
 billTotal = input("What is the total bill?\n")
 billTotalInt = int(billTotal)
 
+tipPercentage = input("What percentage would you like to tip? 10, 12 or 15?\n")
+tipPercentageInt = int(tipPercentage)
